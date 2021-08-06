bc2cc51fa59083ecc8c995bd3e0a889d3b02bebe
MikeYaye <m.moolenaar99@gmail.com>
2021-08-06 23:22:05 +0200

    Replace tabs with spaces

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var browser = browser ?? chrome;
 
 window.onload = function () {
-	// Clone existing menu item and put it at the first position
+    // Clone existing menu item and put it at the first position
     const existingItem = document.querySelector("#header_userpanel_beta > ul > li.userpanel-item.queue")
     const parent = existingItem.parentElement;
     const newItem = parent.insertBefore(existingItem.cloneNode(true), parent.childNodes[0]);
@@ -10,7 +10,7 @@ window.onload = function () {
     newItem.getElementsByClassName("caption")[0].innerText = "Refresh";
     newItem.getElementsByTagName("svg")[0].innerHTML = '<svg id="cr_bookmark_header" viewBox="0 0 48 48"></svg>'
 
-	// Remove existing click events and add new click listener
+    // Remove existing click events and add new click listener
     newItem.childNodes[1].removeAttribute("href")
     newItem.childNodes[1].addEventListener("click", (event) => {
         event.stopImmediatePropagation();
